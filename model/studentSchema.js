8533c16e054f28340bc305ddd06e6118a294d9ff
Ashraf2004 <93571118+Ashraf2004@users.noreply.github.com>
2023-11-03 15:29:38 +0530

    Update studentSchema.js

--- model/studentSchema.js
+++ model/studentSchema.js
@@ -4,7 +4,7 @@ const studentSchema = new mongoose.Schema({
     email: {type: String, unique: true},
     password: String,
 },{
-    collection:"registration"
+    collection:"register"
 })
 
-module.exports = mongoose.model("registration",studentSchema);
+module.exports = mongoose.model("register",studentSchema);
